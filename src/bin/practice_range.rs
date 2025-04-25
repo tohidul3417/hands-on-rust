bae21c31992d9c5ecb66c2142e8ec82a3fe1d474
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2025-04-26 00:10:42 +0600

    feat: practice Range

---
+++ src/bin/practice_range.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for num in 2..5 { // Upto 5.It won't include 5
+        println!("{num}");
+    }
+
+    for num in 10..=15 {
+        println!("{num}");
+    }
+
+    for ch in 'a'..='e' {
+        println!("{ch}");
+    }
+}
